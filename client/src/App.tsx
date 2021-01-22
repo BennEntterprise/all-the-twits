2e785313119b6226e107e69dea9cea01f08d440b
BennEntterprise <kyle@bennentterprise.com>
2021-01-22 14:16:44 -0500

    Use Sample Data in the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+import sampleData from './data/sampleTweets.json'
 
 function App() {
+  const [liveDataPull, setLiveDataPull] = useState(false)
+  const [userToSearch, setUserToSearch] = useState('')
+
+  // 1 Make Request to Twitter API to Obtain the ID Of the Target User
+  // 2 Get the latest tweets from each user
+
+  const getLatestTweets = () => {
+    alert('Ok Searching for ' + userToSearch)
+  }
+
+  console.log(sampleData.data)
+  const tweets = sampleData.data
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>Type the User to Search (without @ sign)</p>
+      <div>
+        <input
+          value={userToSearch}
+          onChange={(e) => setUserToSearch(e.target.value)}
+        />
+        <button onClick={getLatestTweets}>Search</button>
+      </div>
+
+      {tweets.map((tweet: any) => {
+        return (
+          <p>
+            {tweet.created_at}:{tweet.text}
+          </p>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
